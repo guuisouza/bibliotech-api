f757527e479f3d3a249fe7e3fb7c9eb5058fbd3c
Guilherme Souza <thekingguiiz2003@gmail.com>
2025-02-20 17:43:32 -0300

    feat: create author DTO, module, service and controller

---
+++ src/modules/author/author.service.ts
@@ -0,0 +1,52 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException
+} from '@nestjs/common'
+import { PrismaService } from '../prisma/prisma.service'
+import { CreateAuthorDTO } from './dto/create-author.dto'
+
+@Injectable()
+export class AuthorService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: CreateAuthorDTO) {
+    if (data.name.trim() === '') {
+      throw new BadRequestException('name should not be empty')
+    }
+
+    return this.prisma.author.create({ data })
+  }
+
+  async findAll() {
+    const authors = await this.prisma.author.findMany()
+
+    if (!authors) {
+      throw new NotFoundException('authors not found')
+    }
+
+    return authors
+  }
+
+  async findOne(id: number) {
+    await this.checkIfAuthorExists(id)
+
+    return this.prisma.author.findUnique({
+      where: { id }
+    })
+  }
+
+  async delete(id: number) {
+    await this.checkIfAuthorExists(id)
+
+    await this.prisma.author.delete({
+      where: { id }
+    })
+  }
+
+  async checkIfAuthorExists(id: number) {
+    if (!(await this.prisma.author.count({ where: { id } }))) {
+      throw new NotFoundException(`author id ${id} does not exist`)
+    }
+  }
+}
