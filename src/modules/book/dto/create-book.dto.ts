4639277f41658eaa36a0b63c8362fb9693178c90
Guilherme Souza <thekingguiiz2003@gmail.com>
2025-03-14 18:10:56 -0300

    feat: adds new fields in the Create Book DTO

--- src/modules/book/dto/create-book.dto.ts
+++ src/modules/book/dto/create-book.dto.ts
@@ -1,11 +1,43 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator'
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Matches,
+  Max,
+  MaxLength,
+  Min
+} from 'class-validator'
 
 export class CreateBookDTO {
   @IsString()
   @IsNotEmpty()
+  @Matches(/\S/, {
+    message: 'title should not be empty'
+  })
   title: string
 
   @IsNumber()
   @IsNotEmpty()
   authorId: number
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/\S/, {
+    message: 'genre should not be empty'
+  })
+  genre: string
+
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(15)
+  @Matches(/\S/, {
+    message: 'isbn should not be empty'
+  })
+  isbn: string
+
+  @IsNumber()
+  @IsNotEmpty()
+  @Min(1000, { message: 'the year must be 4 digits and valid' })
+  @Max(9999, { message: 'the year must be 4 digits and valid' })
+  yearPublished: number
 }
