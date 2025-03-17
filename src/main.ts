446706bbd723221bf91e9feded6340734051e10c
Guilherme Souza <thekingguiiz2003@gmail.com>
2025-03-17 17:45:45 -0300

    feat: implements the seed service in Prisma module and before starting the server in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
+import { SeedService } from './modules/prisma/seed/seed.service'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
   app.setGlobalPrefix('api/v2')
   app.useGlobalPipes(new ValidationPipe())
+
+  const seedService = app.get(SeedService)
+  await seedService.onModuleInit()
+
   await app.listen(process.env.PORT)
 }
 bootstrap()
