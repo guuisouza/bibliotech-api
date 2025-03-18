3d22c1829e91f0e719bb3b2ebf46d4f08c7dbdb6
Guilherme Souza <thekingguiiz2003@gmail.com>
2025-03-18 16:30:54 -0300

    feat: creates the Guard JWT Auth to secure routes and applies Auth Module in the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AuthorModule } from './modules/author/author.module'
 import { BookModule } from './modules/book/book.module'
 import { StudentModule } from './modules/student/student.module'
 import { LoanModule } from './modules/loan/loan.module'
+import { ConfigModule } from '@nestjs/config'
+import { AuthModule } from './modules/auth/auth.module'
 
 @Module({
-  imports: [AuthorModule, BookModule, StudentModule, LoanModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    AuthorModule,
+    BookModule,
+    StudentModule,
+    LoanModule
+  ],
   controllers: [],
   providers: []
 })
