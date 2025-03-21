6b495a9fd62ace9882943d25b17932460a165046
Guilherme Souza <thekingguiiz2003@gmail.com>
2025-03-21 17:01:41 -0300

    feat: configure swagger in main

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 import { SeedService } from './modules/prisma/seed/seed.service'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -11,6 +12,19 @@ async function bootstrap() {
 
   const seedService = app.get(SeedService)
   await seedService.onModuleInit()
+
+  const config = new DocumentBuilder()
+    .setTitle('Bibliotech API')
+    .setDescription(
+      'API documentation for University Library Management System'
+    )
+    .setVersion('2.0')
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+
+  SwaggerModule.setup('docs', app, document)
 
   await app.listen(process.env.PORT)
 }
