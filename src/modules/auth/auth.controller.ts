8863a650e6d42f12bcc28b52473c0e17b8cde053
Guilherme Souza <thekingguiiz2003@gmail.com>
2025-03-21 18:33:28 -0300

    feat: implements documentation with swagger in the authentication module

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,12 +1,32 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, HttpCode, Post } from '@nestjs/common'
 import { AuthService } from './auth.service'
 import { UserLoginDTO } from './dto/user.login-dto'
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger'
 
+@ApiTags('Authentication')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Admin login' })
+  @ApiResponse({
+    status: 200,
+    description: 'User successfully logged in',
+    schema: {
+      properties: {
+        accessToken: {
+          type: 'string',
+          example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
+        }
+      }
+    }
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized - incorrect email or password'
+  })
   @Post('login')
+  @HttpCode(200)
   async login(@Body() { email, password }: UserLoginDTO) {
     return this.authService.login({ email, password })
   }
--- src/modules/auth/dto/user.login-dto.ts
+++ src/modules/auth/dto/user.login-dto.ts
@@ -1,9 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsEmail, IsString } from 'class-validator'
 
 export class UserLoginDTO {
+  @ApiProperty({
+    example: 'user@example.com',
+    description: 'User email address'
+  })
   @IsEmail()
   email: string
 
+  @ApiProperty({
+    example: 'yourPassword123',
+    description: 'User password'
+  })
   @IsString()
   password: string
 }
